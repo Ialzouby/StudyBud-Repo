ffcd09588b4989c592d9220cfe14183b7bdc6199
ialzouby <ialzouby@gmail.com>
2024-09-14 17:08:47 -0400

    Flas Messages | User Logout | Restricted Pages

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -9,6 +12,31 @@ from .forms import RoomForm
 #     {'id':3, 'name':'Lets learn HTML!'},
 # ]
 
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username = username)
+        except: 
+            messages.error(request, 'User not found')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None: 
+            login(request, user)
+            return redirect('home')
+        else: 
+            messages.error(request, 'Username or Password not found')
+
+    context = {};
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
